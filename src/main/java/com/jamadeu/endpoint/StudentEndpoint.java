46b7c923b62098a139d5034647f28d01f79632e5
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-12 10:29:06 -0300

    Create controller StudentEndpoint

---
+++ src/main/java/com/jamadeu/endpoint/StudentEndpoint.java
@@ -0,0 +1,57 @@
+package com.jamadeu.endpoint;
+
+import com.jamadeu.error.CustomErrorType;
+import com.jamadeu.model.Student;
+import com.jamadeu.repository.IStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * Created by Jean Amadeu 12 julho 2020
+ */
+@RestController
+@RequestMapping("students")
+public class StudentEndpoint {
+
+    private final IStudentRepository studentRepository;
+
+    @Autowired
+    public StudentEndpoint(IStudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listAll() {
+        return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        if (student == null) {
+            return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        return new ResponseEntity<>(studentRepository.save(student), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        studentRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        studentRepository.save(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+}
